f58d38e40cc2b2f55f3ba542f038e6ece8dcd0e0
HarrisonStrand <harrisonstrand@gmail.com>
2021-04-23 12:36:26 -0700

    add routes to play buttons for arcade-page navigation
    Co-Authored-By: Garrett Brown <Gman9mm@live.com>

---
+++ src/app/arcade3/arcade3.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Arcade3Component } from './arcade3.component';
+
+describe('Arcade3Component', () => {
+  let component: Arcade3Component;
+  let fixture: ComponentFixture<Arcade3Component>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ Arcade3Component ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(Arcade3Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/arcade3/arcade3.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-arcade3',
+  templateUrl: './arcade3.component.html',
+  styleUrls: ['./arcade3.component.css']
+})
+export class Arcade3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
